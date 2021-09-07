b91d0aea676934f50fa0f5ea2e807351bf6108bd
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-09-07 21:18:16 +0900

    DistanceTrackerApp: Navigation 구성

--- app/src/main/java/kr/co/bepo/distancetrackerapp/MainActivity.kt
+++ app/src/main/java/kr/co/bepo/distancetrackerapp/MainActivity.kt
@@ -2,10 +2,21 @@ package kr.co.bepo.distancetrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import kr.co.bepo.distancetrackerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navController: NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment).navController  }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
---
+++ app/src/main/java/kr/co/bepo/distancetrackerapp/MapsFragment.kt
@@ -0,0 +1,44 @@
+package kr.co.bepo.distancetrackerapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kr.co.bepo.distancetrackerapp.databinding.FragmentMapsBinding
+
+class MapsFragment : Fragment() {
+
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentMapsBinding.inflate(layoutInflater, container, false)
+        .also { _binding = it }
+        .root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
