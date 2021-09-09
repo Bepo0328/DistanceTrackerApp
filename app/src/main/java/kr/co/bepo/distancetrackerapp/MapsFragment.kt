ef38410177c749825f35c7d92aaa502bce8e4ddf
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-09-09 20:58:12 +0900

    DistanceTrackerApp: Countdown 기능 추가

--- app/src/main/java/kr/co/bepo/distancetrackerapp/MapsFragment.kt
+++ app/src/main/java/kr/co/bepo/distancetrackerapp/MapsFragment.kt
@@ -2,10 +2,12 @@ package kr.co.bepo.distancetrackerapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -19,6 +21,7 @@ import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kr.co.bepo.distancetrackerapp.databinding.FragmentMapsBinding
+import kr.co.bepo.distancetrackerapp.util.ExtensionFunctions.disable
 import kr.co.bepo.distancetrackerapp.util.ExtensionFunctions.hide
 import kr.co.bepo.distancetrackerapp.util.ExtensionFunctions.show
 import kr.co.bepo.distancetrackerapp.util.Permissions.hasBackgroundLocationPermission
@@ -61,16 +64,14 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
     }
 
     override fun onMyLocationButtonClick(): Boolean {
-        binding.apply {
-            hintTextView.animate().alpha(0f).duration = 1500
-            lifecycleScope.launch {
-                delay(2_500L)
-                hintTextView.hide()
-                startButton.show()
-            }
-
-            return false
+        binding.hintTextView.animate().alpha(0f).duration = 1500
+        lifecycleScope.launch {
+            delay(2_500L)
+            binding.hintTextView.hide()
+            binding.startButton.show()
         }
+
+        return false
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -89,10 +90,35 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
 
     private fun onStartButtonClicked() {
         if (hasBackgroundLocationPermission(requireContext())) {
-            Log.d("MapsActivity", "Already Enabled.")
+            startCountDown()
+            binding.startButton.disable()
+            binding.startButton.hide()
+            binding.stopButton.show()
         } else {
             requestBackgroundLocationPermission(this)
         }
+    }
+
+    private fun startCountDown() {
+        binding.timerTextView.show()
+        binding.stopButton.disable()
+        val timer: CountDownTimer = object : CountDownTimer(4_000L, 1_000L) {
+            override fun onTick(millisUnitFinished: Long) {
+                val currentSecond = millisUnitFinished / 1_000
+                if (currentSecond.toString() == "0") {
+                    binding.timerTextView.text = "GO"
+                    binding.timerTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                } else {
+                    binding.timerTextView.text = currentSecond.toString()
+                    binding.timerTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
+                }
+            }
+
+            override fun onFinish() {
+                binding.timerTextView.hide()
+            }
+        }
+        timer.start()
     }
 
     override fun onRequestPermissionsResult(
