ee057299078bebb60bc2934f943a71735d338994
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-09-09 20:49:59 +0900

    DistanceTrackerApp: MyLocation 및 Permission 추가

--- app/src/main/java/kr/co/bepo/distancetrackerapp/MapsFragment.kt
+++ app/src/main/java/kr/co/bepo/distancetrackerapp/MapsFragment.kt
@@ -1,22 +1,35 @@
 package kr.co.bepo.distancetrackerapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kr.co.bepo.distancetrackerapp.databinding.FragmentMapsBinding
+import kr.co.bepo.distancetrackerapp.util.ExtensionFunctions.hide
+import kr.co.bepo.distancetrackerapp.util.ExtensionFunctions.show
+import kr.co.bepo.distancetrackerapp.util.Permissions.hasBackgroundLocationPermission
+import kr.co.bepo.distancetrackerapp.util.Permissions.requestBackgroundLocationPermission
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
+class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, EasyPermissions.PermissionCallbacks {
 
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,8 +44,33 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         _binding = null
     }
 
-    override fun onMapReady(mMap: GoogleMap) {
-        
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.isMyLocationEnabled = true
+        mMap.setOnMyLocationButtonClickListener(this)
+
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = false
+            isZoomGesturesEnabled = false
+            isRotateGesturesEnabled = false
+            isTiltGesturesEnabled = false
+            isCompassEnabled = false
+            isScrollGesturesEnabled = false
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        binding.apply {
+            hintTextView.animate().alpha(0f).duration = 1500
+            lifecycleScope.launch {
+                delay(2_500L)
+                hintTextView.hide()
+                startButton.show()
+            }
+
+            return false
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,8 +82,36 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     }
 
     private fun initButton() = with(binding) {
-        startButton.setOnClickListener {  }
+        startButton.setOnClickListener { onStartButtonClicked() }
         stopButton.setOnClickListener {  }
         resetButton.setOnClickListener {  }
+    }
+
+    private fun onStartButtonClicked() {
+        if (hasBackgroundLocationPermission(requireContext())) {
+            Log.d("MapsActivity", "Already Enabled.")
+        } else {
+            requestBackgroundLocationPermission(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(requireActivity()).build().show()
+        } else {
+            requestBackgroundLocationPermission(this)
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        onStartButtonClicked()
     }
 }
--- app/src/main/java/kr/co/bepo/distancetrackerapp/util/Constants.kt
+++ app/src/main/java/kr/co/bepo/distancetrackerapp/util/Constants.kt
@@ -3,4 +3,6 @@ package kr.co.bepo.distancetrackerapp.util
 object Constants {
 
     const val PERMISSION_LOCATION_REQUEST_CODE = 1
+    const val PERMISSION_BACKGROUND_LOCATION_REQUEST_CODE = 2
+
 }
